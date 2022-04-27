61015c6771f349a920621ca1270779d69ed87616
Nueton Huynh <nhuynh@Nuetons-MacBook-Pro.local>
2022-04-27 00:41:29 -0700

    added simple socket app

---
+++ SocketApp/SocketApp/SocketAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SocketAppApp.swift
+//  SocketApp
+//
+//  Created by Nueton Huynh on 4/19/22.
+//
+
+import SwiftUI
+
+@main
+struct SocketAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
