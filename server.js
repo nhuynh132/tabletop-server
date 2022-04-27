92d97c9950c73d4cc1c1bac4398133da6e44005a
Royce-Work Computer <roycekim99@users.noreply.github.com>
2022-04-27 05:55:29 -0700

    port def 3001

--- server.js
+++ server.js
@@ -2,11 +2,12 @@ var express = require("express");
 var app = express();
 var server = require("http").createServer(app);
 var io = require("socket.io")(server);
+var PORT = 3001;
 
 connections = [];
 
-server.listen(process.env.PORT || 3001);
-console.log("Server is running...");
+server.listen(PORT);
+console.log("Server is running on ${PORT}...");
 
 io.sockets.on("connection", function (socket) {
   connections.push(socket);
