a257f560879e0466ec690c848084234fa8e7f9b7
Royce-Work Computer <roycekim99@users.noreply.github.com>
2022-04-27 05:39:24 -0700

    change port to 3001

--- server.js
+++ server.js
@@ -1,33 +1,37 @@
-var express = require('express');
+var express = require("express");
 var app = express();
-var server = require('http').createServer(app);
-var io = require('socket.io')(server);
+var server = require("http").createServer(app);
+var io = require("socket.io")(server);
 
 connections = [];
 
-server.listen(process.env.PORT || 3000);
-console.log('Server is running...');
+server.listen(process.env.PORT || 3001);
+console.log("Server is running...");
 
+io.sockets.on("connection", function (socket) {
+  connections.push(socket);
+  console.log("Connect: %s sockets are connected", connections.length);
 
+  // TODO: model-place
+  socket.on("model-placed", function (data) {
+    io.emit("model-placed", data);
+    console.log("Model has been placed: \n", data);
+  });
 
-io.sockets.on('connection', function(socket) {
-    connections.push(socket);
-    console.log('Connect: %s sockets are connected', connections.length);
+  // TODO: model-transformed
+  socket.on("model-transformed", function (data) {
+    io.emit("model-transformed", data);
+    console.log("Model has transformed: \n", data);
+  });
 
-    // Received a model
-    socket.on('model-data', function(data) {
-        io.emit('model-data', data)
-        console.log('Model data received: \n', data)
-    });
+  // Disconnect
+  socket.on("disconnect", function (data) {
+    connections.splice(connections.indexOf(socket), 1);
+    console.log("Connect: %s sockets are connected", connections.length);
+  });
 
-    // Disconnect 
-    socket.on('disconnect', function(data) {
-        connections.splice(connections.indexOf(socket), 1);
-        console.log('Connect: %s sockets are connected', connections.length);
-    });
-
-    socket.on('NodeJS Server Port', function(data) {
-        console.log(data);
-        io.sockets.emit('iOS Client Port', {msg: 'Hi iOS Client.'});
-    });
+  socket.on("NodeJS Server Port", function (data) {
+    console.log(data);
+    io.sockets.emit("iOS Client Port", { msg: "Hi iOS Client." });
+  });
 });
